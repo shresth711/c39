6b2264f55342c84df3f06afbca3358bb76b65326
shresth711 <gupta.shresth864p@gmail.com>
2021-06-19 13:43:09 +0530

    ad

--- sketch.js
+++ sketch.js
@@ -9,7 +9,16 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var car1_img,car2_img,car3_img,car4_img,track,ground;
 
+function preload(){
+  car1_img = loadImage("images/car1.png");
+  car2_img = loadImage("images/car2.png");
+  car3_img = loadImage("images/car3.png");
+  car4_img = loadImage("images/car4.png");
+  track = loadImage("images/track.png");
+  ground = loadImage("images/ground.png")
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +36,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end();
   }
 }
